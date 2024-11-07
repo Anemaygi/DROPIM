b65a2821ad51146aa40dc4cb3239c56eaefc8f71
segalv <anaclarasvp@usp.br>
2024-11-07 13:34:25 -0300

    Reorganizando divisão de diretórios

--- ESP32/codigo.py
+++ ESP32/codigo.py
@@ -4,7 +4,7 @@ trct = machine.ADC(machine.Pin(34))
 led = machine.Pin(2, machine.Pin.OUT)
 
 while True:
-    if trct < 2000:
+    if trct.read() < 2000:
         led.on()
     else:
         led.off()
